2de7e5d5c99ad1156fb593c90e3edf879685efd7
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-07-28 00:15:36 -0400

    added types to the pokemon cards FEAT. AngelDelunadev

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,22 @@
 function PokemonCard(props) {
     return (
         <>
-        <div id="pokemonDiv">
-        <h3>{props.pokemon.name}</h3>
-        <div>
-        <img src={props.pokemon.sprites.front_default} alt={props.pokemon.name}></img>
-        <img src={props.pokemon.sprites.front_shiny} alt={props.pokemon.name}></img>
-        </div>
-        <p>National Pokedex ID: {props.pokemon.id}</p>
-        </div>
+            <div id="pokemonDiv">
+                <h3>{props.pokemon.name}</h3>
+                <div>
+                    <img src={props.pokemon.sprites.front_default} alt={props.pokemon.name}></img>
+                    <img src={props.pokemon.sprites.front_shiny} alt={props.pokemon.name}></img>
+                </div>
+                <h4>Types</h4>
+                <div>
+                    {props.pokemon.types.map(type => {
+                        return (
+                            <p>{type.type.name}</p>
+                        )
+                    })}
+                </div>
+                <p>National Pokedex ID: {props.pokemon.id}</p>
+            </div>
         </>
     )
 }
